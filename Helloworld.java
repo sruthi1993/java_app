93795ba82a5b75e5a5e04dc6eb562042a3f5c1d8
sruthi1993 <sruthi073@gmail.com>
2018-03-11 05:22:36 -0400

    On branch master  Initial commit  Changes to be committed:
    	new file:   Helloworld.class
    	new file:   Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+public class Helloworld {
+    public static void main( String[] args ) {
+        System.out.println("Hello, my name is Noah!");
+        System.out.println("I will be your virtual assistant!");
+        System.out.println("Please, be my guest");
+        
+        System.exit(0);
+    }
+}
